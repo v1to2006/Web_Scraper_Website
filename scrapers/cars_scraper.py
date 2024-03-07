4b88b1abde2906c0b3d4c88aa0df30b20da71bed
v1to <pixel2006.aa@gmail.com>
2024-03-07 09:00:18 +0200

    Update cars_scraper.py

--- scrapers/cars_scraper.py
+++ scrapers/cars_scraper.py
@@ -3,35 +3,36 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 
 class Car:
-    def __init__(self, make, model, year, mileage, price, url):
+    def __init__(self, make, model, year, mileage, price, img, url):
         self.make = make
         self.model = model
         self.year = year
         self.mileage = mileage
         self.price = price
+        self.img = img
         self.url = url
 
-def find_cars():
+def find_cars(url):
     cars = []
 
     with webdriver.Chrome() as driver:
-        url = 'https://www.nettiauto.com/vaihtoautot?sortCol=enrolldate&ord=DESC&page='
-
         driver.get(url)
         time.sleep(0.1)
         soup = BeautifulSoup(driver.page_source, "html.parser")
 
         car_elements = soup.find_all("a", class_="childVifUrl tricky_link")
+        car_images = soup.find_all("img", { "border" : "0"})
         
-        for car in car_elements:
+        for car, img in zip(car_elements, car_images):
             current_make = car["data-make"]
             current_model = car["data-model"]
             current_year = int(car["data-year"])
             current_mileage = int(car["data-mileage"])
             current_price = int(car["data-price"])
+            current_img = img["data-src"]
             current_url = car["href"]
             
-            cars.append(Car(current_make, current_model, current_year, current_mileage, current_price, current_url))
+            cars.append(Car(current_make, current_model, current_year, current_mileage, current_price, current_img, current_url))
 
     return cars
 
@@ -42,7 +43,13 @@ def save_to_json(filename):
        
         print(f"Cars results saved to {filename}")
 
-car_list = find_cars()
+car_list = []
+
+for i in range(10):
+    url = f'https://www.nettiauto.com/vaihtoautot?pfrom=5000&pto=75000&id_country[]=73&page={i + 1}'
+    
+    for car in find_cars(url):
+        car_list.append(car)
 
 save_to_json("C:/GitHub/Web/Web_Scraper_Project/Web_Scraper_Website/scrapers/results/cars_results.json")
 
